eccff685181184bbc9fb5e12b767bd87758510f5
hhhoang.h3@gmail.com <hhhoang.h3@gmail.com>
2024-01-18 15:40:54 +0700

    update

--- src/components/pages/home/c/dataC.js
+++ src/components/pages/home/c/dataC.js
@@ -9,7 +9,7 @@ export const dataC = [
         id: '02',
         text: 'Thiết kế',
         content:
-           'Chúng tôi không chỉ đơn thuần là những chuyên gia tư vấn, mà còn là đối tác chiến lược đồng hành với khách hàng. Tư vấn của chúng tôi không chỉ giúp hình thành ý tưởng mà còn xác định chiến lược và hướng phát triển chi tiết cho mọi dự án.'
+           'Với sự sáng tạo độc đáo và tận tâm, chúng tôi không chỉ tạo ra các bản vẽ mà còn xây dựng những không gian sống và làm việc có tính thực hiện cao. Mỗi thiết kế của chúng tôi không chỉ là kết quả của sự sáng tạo, mà còn phản ánh đúng nhu cầu và phong cách cá nhân của khách hàng.'
     },
     {
         id: '03',
@@ -21,12 +21,12 @@ export const dataC = [
         id: '04',
         text: 'Bảo trì và dịch vụ sau bán hàng',
         content:
-           'Chúng tôi không chỉ đơn thuần là những chuyên gia tư vấn, mà còn là đối tác chiến lược đồng hành với khách hàng. Tư vấn của chúng tôi không chỉ giúp hình thành ý tưởng mà còn xác định chiến lược và hướng phát triển chi tiết cho mọi dự án.'
+           'Chúng tôi không chỉ dừng lại sau khi dự án hoàn thành mà còn duy trì và nâng cấp không gian theo thời gian. Đội ngũ bảo trì của chúng tôi sẵn sàng hỗ trợ và giải quyết mọi vấn đề để đảm bảo sự hài lòng và lâu dài cho khách hàng.'
     },
      {
         id: '05',
         text: 'Nghiên cứu và phát triển',
         content:
-           'Chúng tôi không chỉ đơn thuần là những chuyên gia tư vấn, mà còn là đối tác chiến lược đồng hành với khách hàng. Tư vấn của chúng tôi không chỉ giúp hình thành ý tưởng mà còn xác định chiến lược và hướng phát triển chi tiết cho mọi dự án.'
+           'Chúng tôi không ngừng đầu tư vào nghiên cứu và phát triển, áp dụng công nghệ mới và xu hướng sáng tạo nhất trong ngành. Điều này giúp chúng tôi không chỉ giữ vững vị thế hàng đầu mà còn mang đến những giải pháp kiến trúc và thiết kế tiên tiến, đáp ứng đúng và vượt qua mong đợi của khách hàng.'
     },
 ]
